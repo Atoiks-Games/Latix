be7b2cced512d6b1c5647e581afb6047a66bcc62
PerenDarksbane <ajbimm@gmail.com>
2017-11-14 18:11:45 -0500

    Adding Files to Repo
    Added Core Files
    Changed File Names

---
+++ src/main/java/org/atoiks/Latix/App.java
@@ -0,0 +1,291 @@
+package org.atoiks.wchess;
+
+import org.lwjgl.*;
+import org.lwjgl.glfw.*;
+import org.lwjgl.opengl.*;
+import org.lwjgl.system.*;
+
+import java.nio.*;
+
+import static org.lwjgl.glfw.Callbacks.*;
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.system.MemoryStack.*;
+import static org.lwjgl.system.MemoryUtil.*;
+
+public class App {
+
+    private static final int HEIGHT = 300;
+    private static final int WIDTH = 300;
+
+    private static final byte INITING =  0b0000;
+    private static final byte WAIT_FLG = 0b0001;
+    private static final byte DEST_FLG = 0b1000;
+    private static final byte P1_FLG =   0b0010;
+    private static final byte P2_FLG =   0b0100;
+    
+    private static final byte P1_TURN0 = P1_FLG;
+    private static final byte P1_TURN1 = P1_FLG | DEST_FLG;
+    private static final byte P2_TURN0 = P2_FLG;
+    private static final byte P2_TURN1 = P2_FLG | DEST_FLG;
+
+    private Board board = new Board();
+    private long window;
+
+    private byte state = INITING;
+
+    private int mouseVirtX = 0;
+    private int mouseVirtY = 0;    
+
+    private int originX = 0;
+    private int originY = 0;
+
+    public void run() {
+	init();
+	loop();
+
+	glfwFreeCallbacks(window);
+	glfwDestroyWindow(window);
+
+	glfwTerminate();
+	glfwSetErrorCallback(null).free();
+    }
+
+    public void init() {
+	GLFWErrorCallback.createPrint(System.err).set();
+	if (!glfwInit()) throw new IllegalStateException("Cannot init GLFW");
+
+	glfwDefaultWindowHints();
+	glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+	window = glfwCreateWindow(300, 300, "Chess?", NULL, NULL);
+	if (window == NULL) throw new RuntimeException("Failed to crete GLFW window");
+
+	glfwSetMouseButtonCallback(window, (window, button, action, mods) -> {
+		if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS) {
+		    if ((state & WAIT_FLG) != WAIT_FLG) return;
+		    
+		    try (MemoryStack stack = stackPush()) {
+			final DoubleBuffer xptr = stack.mallocDouble(1);
+			final DoubleBuffer yptr = stack.mallocDouble(1);
+			glfwGetCursorPos(window, xptr, yptr);
+
+			final double x = xptr.get(0);
+			final double y = yptr.get(0);
+			if (!(x <= 20 || x >= 280 || y <= 20 || y >= 280)) {
+			    mouseVirtX = (int) ((x - 20) / 28);
+			    mouseVirtY = (int) ((y - 20) / 28);
+			    state ^= WAIT_FLG;
+			}
+		    }
+		}
+	    });
+	
+	final GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+	glfwSetWindowPos(window,
+			 (vidmode.width() - WIDTH) / 2,
+			 (vidmode.height() - HEIGHT) / 2);
+
+	glfwMakeContextCurrent(window);
+	glfwSwapInterval(GLFW_TRUE);
+	glfwShowWindow(window);
+
+	board.reset();
+    }
+
+    private void loop() {
+	GL.createCapabilities();
+	glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
+
+	state = P1_TURN0 | WAIT_FLG;
+
+	while (!glfwWindowShouldClose(window)) {
+	    glfwPollEvents();
+	    update();
+	    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	    render();
+	    glfwSwapBuffers(window);
+	}
+    }
+
+    private void update() {
+        switch (board.getWinner()) {
+	case 1:
+	case 2:
+	    glfwSetWindowShouldClose(window, true);
+	    return;
+	}
+
+	switch (state) {
+	case P1_TURN0:
+	    if (board.hasP1Piece(mouseVirtX, mouseVirtY)) {
+		originX = mouseVirtX;
+		originY = mouseVirtY;
+		state = P1_TURN1;
+	    }
+	    state |= WAIT_FLG;
+	    return;
+	case P1_TURN1: {
+	    final int[] validTiles = board.getMovementOptions(originX, originY, 1);
+	    for (int i = 0; i < validTiles.length; i += 2) {
+		if (mouseVirtX == validTiles[i] && mouseVirtY == validTiles[i + 1]) {
+		    board.bruteMove(originX, originY, mouseVirtX, mouseVirtY);
+		    state = P2_TURN0 | WAIT_FLG;
+		    return;
+		}
+	    }
+
+	    state |= WAIT_FLG;
+	    return;
+	}
+	case P2_TURN0:
+	    if (board.hasP2Piece(mouseVirtX, mouseVirtY)) {
+		originX = mouseVirtX;
+		originY = mouseVirtY;
+		state = P2_TURN1;
+	    }
+	    state |= WAIT_FLG;
+	    return;
+	case P2_TURN1: {
+	    final int[] validTiles = board.getMovementOptions(originX, originY, 2);
+	    for (int i = 0; i < validTiles.length; i += 2) {
+		if (mouseVirtX == validTiles[i] && mouseVirtY == validTiles[i + 1]) {
+		    board.bruteMove(originX, originY, mouseVirtX, mouseVirtY);
+		    state = P1_TURN0 | WAIT_FLG;
+		    return;
+		}
+	    }
+
+	    state |= WAIT_FLG;
+	    return;
+	}
+	}
+    }
+
+    private void render() {
+	glColor3f(0.5f, 0.5f, 0.5f);
+	glBegin(GL_LINES);
+	{
+	    // Horizontal lines
+	    glVertex2f(-0.8f, 0.8f);
+	    glVertex2f(0.8f, 0.8f);
+	    glVertex2f(-0.8f, 0.6f);
+	    glVertex2f(0.8f, 0.6f);
+	    glVertex2f(-0.8f, 0.4f);
+	    glVertex2f(0.8f, 0.4f);
+	    glVertex2f(-0.8f, 0.2f);
+	    glVertex2f(0.8f, 0.2f);
+	    glVertex2f(-0.8f, 0.0f);
+	    glVertex2f(-0.4f, 0.0f);
+	    glVertex2f(0.4f, 0.0f);
+	    glVertex2f(0.8f, 0.0f);
+	    glVertex2f(-0.8f, -0.2f);
+	    glVertex2f(0.8f, -0.2f);
+	    glVertex2f(-0.8f, -0.4f);
+	    glVertex2f(0.8f, -0.4f);
+	    glVertex2f(-0.8f, -0.6f);
+	    glVertex2f(0.8f, -0.6f);
+	    glVertex2f(-0.8f, -0.8f);
+	    glVertex2f(0.8f, -0.8f);
+
+	    // Vertical lines
+	    glVertex2f(-0.8f, -0.8f);
+	    glVertex2f(-0.8f, 0.8f);
+	    glVertex2f(-0.6f, -0.8f);
+	    glVertex2f(-0.6f, 0.8f);
+	    glVertex2f(-0.4f, -0.8f);
+	    glVertex2f(-0.4f, 0.8f);
+	    glVertex2f(-0.2f, -0.8f);
+	    glVertex2f(-0.2f, -0.2f);
+	    glVertex2f(-0.2f, 0.2f);
+	    glVertex2f(-0.2f, 0.8f);
+	    glVertex2f(0.0f, -0.8f);
+	    glVertex2f(0.0f, -0.2f);
+	    glVertex2f(0.0f, 0.2f);
+	    glVertex2f(0.0f, 0.8f);
+	    glVertex2f(0.2f, -0.8f);
+	    glVertex2f(0.2f, -0.2f);
+	    glVertex2f(0.2f, 0.2f);
+	    glVertex2f(0.2f, 0.8f);
+	    glVertex2f(0.4f, -0.8f);
+	    glVertex2f(0.4f, 0.8f);
+	    glVertex2f(0.6f, -0.8f);
+	    glVertex2f(0.6f, 0.8f);
+	    glVertex2f(0.8f, -0.8f);
+	    glVertex2f(0.8f, 0.8f);
+
+	    // Diagonal lines
+	    glVertex2f(-0.8f, -0.6f);
+	    glVertex2f(-0.4f, -0.2f);
+	    glVertex2f(-0.6f, -0.8f);
+	    glVertex2f(0.0f, -0.2f);
+	    glVertex2f(0.2f, -0.8f);
+	    glVertex2f(0.8f, -0.2f);
+	    
+	    glVertex2f(0.8f, -0.6f);
+	    glVertex2f(0.4f, -0.2f);
+	    glVertex2f(0.6f, -0.8f);
+	    glVertex2f(0.0f, -0.2f);
+	    glVertex2f(-0.2f, -0.8f);
+	    glVertex2f(-0.8f, -0.2f);
+	    
+	    glVertex2f(-0.8f, 0.6f);
+	    glVertex2f(-0.4f, 0.2f);
+	    glVertex2f(-0.6f, 0.8f);
+	    glVertex2f(0.0f, 0.2f);
+	    glVertex2f(0.2f, 0.8f);
+	    glVertex2f(0.8f, 0.2f);
+	    
+	    glVertex2f(0.8f, 0.6f);
+	    glVertex2f(0.4f, 0.2f);
+	    glVertex2f(0.6f, 0.8f);
+	    glVertex2f(0.0f, 0.2f);
+	    glVertex2f(-0.2f, 0.8f);
+	    glVertex2f(-0.8f, 0.2f);
+	}
+	glEnd();
+
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	glBegin(GL_QUADS);
+	float startX = -0.8f;
+	for (int i = 0; i < Board.DIMENSION; ++i) {
+	    float startY = -0.8f;
+	    // Draw the board bottom-up (board is top-down)
+	    for (int j = Board.DIMENSION - 1; j >= 0; --j) {
+		if (board.hasP1Piece(i, j)) {
+		    if (originX == i && originY == j
+			&& (state & DEST_FLG) == DEST_FLG) {
+			glColor4f(1.0f, 0.0f, 0.0f, 0.25f);
+		    } else {
+			glColor4f(1.0f, 0.0f, 0.0f, 1.0f);
+		    }
+		    glVertex2f(startX - 0.075f, startY + 0.075f);
+		    glVertex2f(startX - 0.075f, startY - 0.075f);
+		    glVertex2f(startX + 0.075f, startY - 0.075f);
+		    glVertex2f(startX + 0.075f, startY + 0.075f);
+		} else if (board.hasP2Piece(i, j)) {
+		    if (originX == i && originY == j
+			&& (state & DEST_FLG) == DEST_FLG) {
+			glColor4f(0.0f, 0.5f, 1.0f, 0.25f);
+		    } else {
+			glColor4f(0.0f, 0.5f, 1.0f, 1.0f);
+		    }
+		    glVertex2f(startX - 0.075f, startY + 0.075f);
+		    glVertex2f(startX - 0.075f, startY - 0.075f);
+		    glVertex2f(startX + 0.075f, startY - 0.075f);
+		    glVertex2f(startX + 0.075f, startY + 0.075f);
+		}
+		startY += 0.2f;
+	    }
+	    startX += 0.2f;
+	}
+	glEnd();
+	glDisable(GL_BLEND);
+    }
+    
+    public static void main(String[] args) {
+        new App().run();
+    }
+}
---
+++ src/main/java/org/atoiks/Latix/Board.java
@@ -0,0 +1,280 @@
+package org.atoiks.wchess;
+
+public final class Board implements java.io.Serializable {
+    private enum TileState {
+	EMPTY, P1_PIECE, P2_PIECE, RESERVED
+    }
+
+    private static final long serialVersionUID = 1279125383L;
+    public static final int DIMENSION = 9;
+    
+    private final TileState[][] tiles;
+    
+    public Board() {
+	tiles = new TileState[DIMENSION][DIMENSION];
+    }
+
+    public void reset() {
+	for (int i = 0; i < DIMENSION; ++i) {
+	    for (int j = 0; j < DIMENSION; ++j) {
+		// null tiles indicate non-passable tiles
+		if (i == 4 && j >= 3 && j <= 5) {
+		    tiles[i][j] = TileState.RESERVED;
+		    continue;
+		}
+
+		// P1 set up
+		if (i == 0 && j >= 1 && j <= 7) {
+		    tiles[i][j] = j == 4 ? TileState.RESERVED : TileState.P1_PIECE;
+		    continue;
+		}
+		if (i == 1 && j >= 2 && j <= 6) {
+		    tiles[i][j] = TileState.P1_PIECE;
+		    continue;
+		}
+		
+		// P2 set up
+		if (i == 8 && j >= 1 && j <= 7) {
+		    tiles[i][j] = j == 4 ? TileState.RESERVED : TileState.P2_PIECE;
+		    continue;
+		}
+		if (i == 7 && j >= 2 && j <= 6) {
+		    tiles[i][j] = TileState.P2_PIECE;
+		    continue;
+		}
+		tiles[i][j] = TileState.EMPTY;
+	    }
+	}
+    }
+
+    public static boolean isRegularTile(int x, int y) {
+	// isNonPassableTile will handle out of bound coordinates
+	return !(isNonPassableTile(x, y) || isDiagonalTile(x, y));
+    }
+
+    public static boolean isNonPassableTile(int x, int y) {
+	if (x < 0 || x >= DIMENSION || y < 0 || y >= DIMENSION) {
+	    // out of bounds, consider it non-passable
+	    return true;
+	}
+	
+        return (y == 4 && (x == 3 || x == 4 || x == 5))
+	    || isSpawnerTile(x, y);
+    }
+
+    public static boolean isSpawnerTile(int x, int y) {
+	return x == 4 && (y == 0 || y == 8);
+    }
+
+    public static boolean isDiagonalTile(int x, int y) {
+	return ((y == 1 || y == 7) && (x == 2 || x == 6))
+	    || ((y == 2 || y == 6) && (x == 1 || x == 7))
+	    || ((y == 3 || y == 5) && x == 4);
+    }
+
+    /**
+     * Checks to see if there is a winner
+     *
+     * @returns 1 if P1 won, 2 if P2 won, 0 if draw,
+     *          anything else if no one won
+     */
+    public int getWinner() {
+	// These do not include the ones on the 'o' tiles
+	int p1RemPieces = 0;
+	int p2RemPieces = 0;
+
+        boolean p1HasPossibleMoves = true;
+        boolean p2HasPossibleMoves = true;
+	
+	for (int y = 0; y < DIMENSION; ++y) {
+	    for (int x = 0; x < DIMENSION; ++x) {
+		if (!isNonPassableTile(x, y)) {
+		    switch (tiles[y][x]) {
+		    case P1_PIECE:
+			++p1RemPieces;
+			if (p1HasPossibleMoves) {
+			    final int[] tiles = getMovementOptions(x, y, 1);
+			    for (int i = tiles.length; i > 0; --i) {
+				if (tiles[i - 1] < 0) {
+				    p1HasPossibleMoves = false;
+				    break;
+				}
+			    }
+			}
+			break;
+		    case P2_PIECE:
+			++p2RemPieces;
+			if (p2HasPossibleMoves) {
+			    final int[] tiles = getMovementOptions(x, y, 2);
+			    for (int i = tiles.length; i > 0; --i) {
+				if (tiles[i - 1] < 0) {
+				    p2HasPossibleMoves = false;
+				    break;
+				}
+			    }
+			}
+			break;
+		    }
+		}
+	    }
+	}
+
+	if (p1RemPieces == 0) {
+	    // check if p2 also has no more pieces (draw)
+	    return p2RemPieces == 0 ? 0 : 2;
+	}
+	if (p2RemPieces == 0) {
+	    return 1;
+	}
+
+	if (!p1HasPossibleMoves) {
+	    // check if p2 also has no more possible moves (draw)
+	    return p2HasPossibleMoves ? 2 : 0;
+	}
+	if (!p2HasPossibleMoves) {
+	    return 1;
+	}
+
+	// anything but 0, 1, 2 will work
+	return -1;
+    }
+
+    public boolean hasP1Piece(int x, int y) {
+	return tiles[y][x] == TileState.P1_PIECE;
+    }
+
+    public boolean hasP2Piece(int x, int y) {
+	return tiles[y][x] == TileState.P2_PIECE;
+    }
+
+    private boolean isBlocked(int x, int y, int playerView) {
+	// isNonPassableTile handles out of bound coordinates
+	if (isNonPassableTile(x, y)) {
+	    return true;
+	}
+        switch (playerView) {
+	case 1:
+	    return tiles[y][x] == TileState.P1_PIECE;
+	case 2:
+	    return tiles[y][x] == TileState.P2_PIECE;
+	default:
+	    return tiles[y][x] != TileState.EMPTY;
+	}
+    }
+
+    public int[] getMovementOptions(int x, int y, int playerView) {
+	// The result is in format of [x1, y1, x2, y2, ..., xn, yn]
+	// if movement is impossible (or the tile could not contain
+	// a piece) an empty array is returned
+	//
+	// It may return negative coordinates. These coordinates
+	// are considered invalid.
+
+	int[] aug = new int[0];
+	if (isSpawnerTile(x, y) || isRegularTile(x, y)) {
+	    // acts like a regular tile
+	    // up, down    -> 1 or 2 tiles
+	    // left, right -> 1 tiles
+	    
+	    aug = new int[] {
+		x - 1, y,	// left
+		x + 1, y,	// right
+		x, y + 1, x, y + 2, // up * 2
+		x, y - 1, x, y - 2  // down * 2
+	    };
+	} else if (isDiagonalTile(x, y)) {
+	    switch (y) {
+	    case 1:
+		aug = new int[] {
+		    x - 1, 0, x + 1, 0,
+		    x - 1, 2, x + 1, 2,
+		    x - 2, 3, x + 2, 3
+		};
+		break;
+	    case 7:
+		aug = new int[] {
+		    x - 2, 5, x + 2, 5,
+		    x - 1, 6, x + 1, 6,
+		    x - 1, 8, x + 1, 8
+		};
+		break;
+	    case 2:
+		aug = new int[] {
+		    x - 1, 1, x + 1, 1,
+		    x - 1, 3, x + 1, 3,
+		    x - 2, 0, x + 2, 0
+		};
+		break;
+	    case 6:
+		aug = new int[] {
+		    x - 1, 7, x + 1, 7,
+		    x - 1, 5, x + 1, 5,
+		    x - 2, 8, x + 2, 8
+		};
+		break;
+	    case 3:
+		aug = new int[] {
+		    x - 1, 2, x + 1, 2,
+		    x - 2, 1, x + 2, 1,
+		    x - 3, 0, x + 3, 0
+		};
+		break;
+	    case 5:
+		aug = new int[] {
+		    x - 1, 6, x + 1, 6,
+		    x - 2, 7, x + 2, 7,
+		    x - 3, 8, x + 3, 8
+		};
+		break;
+	    }
+	    
+	    if (aug.length == 0) {
+		throw new AssertionError("Case of " + x + "," + y + " not handled!");
+	    }
+	}
+
+	for (int i = 0; i < aug.length; i += 2) {
+	    if (isBlocked(aug[i], aug[i + 1], playerView)) {
+		aug[i] = aug[i + 1] = -1;
+	    }
+	}
+	return aug;
+    }
+
+    public void bruteMove(int x1, int y1, int x2, int y2) {
+	// Paths are not validated in this method
+	// just make sure it is a piece being moved
+	// (no RESERVED / EMPTY moving) and that
+	// its not moving to itself
+	if (x1 == x2 && y1 == y2) return;
+	switch (tiles[y1][x1]) {
+	case P1_PIECE:
+	case P2_PIECE:
+	    tiles[y2][x2] = tiles[y1][x1];
+	    tiles[y1][x1] = isSpawnerTile(x1, y1) ? TileState.RESERVED : TileState.EMPTY;
+	    break;
+	}
+    }
+
+    public void print(final java.io.PrintStream out) {
+	for (int i = 0; i < DIMENSION; ++i) {
+	    for (int j = 0; j < DIMENSION; ++j) {
+		switch (tiles[i][j]) {
+		case P1_PIECE:
+		    out.print('1');
+		    break;
+		case P2_PIECE:
+		    out.print('2');
+		    break;
+		case EMPTY:
+		    out.print('+');
+		    break;
+		case RESERVED:
+		    out.print('x');
+		    break;
+		}
+	    }
+	    out.println("");
+	}
+    }
+}
